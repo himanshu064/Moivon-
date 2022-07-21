9d0e8429d4288dca068193caf9f31f6875fd0a73
himanshu kumar <himanshu064@gmail.com>
2022-07-21 17:42:25 +0530

    changes in event page

--- src/pages/all-event/index.js
+++ src/pages/all-event/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Event from "../../components/Event";
 import styles from "./index.module.css";
 
@@ -6,6 +6,9 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Heading from "../../components/Heading";
+import Button from "../../components/Button";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
 const eventData = [
   {
@@ -116,6 +119,8 @@ const eventData = [
 ];
 
 function AllEvent() {
+  const [key, setKey] = useState("home");
+
   return (
     <>
       <section className="section">
@@ -124,15 +129,74 @@ function AllEvent() {
             <Col md={12}>
               <div className={styles.navHead}>
                 <Heading>Events</Heading>
+                <Button variant="outline">Sort</Button>
               </div>
             </Col>
           </Row>
           <Row>
-            {eventData?.map((data) => (
-              <Col md={4} className="mb-3">
-                <Event event={data} />
-              </Col>
-            ))}
+            <Col>
+              <Tabs
+                id="controlled-tab-example"
+                activeKey={key}
+                onSelect={(k) => setKey(k)}
+                className="mb-3"
+              >
+                <Tab eventKey="events" title="All events">
+                  <Row>
+                    {eventData?.map((data) => (
+                      <Col md={4} className="mb-3">
+                        <Event event={data} />
+                      </Col>
+                    ))}
+                  </Row>
+                </Tab>
+                <Tab eventKey="classic" title="CLASSIC MUSEUM">
+                  <Row>
+                    {eventData?.map((data) => (
+                      <Col md={4} className="mb-3">
+                        <Event event={data} />
+                      </Col>
+                    ))}
+                  </Row>
+                </Tab>
+                <Tab eventKey="gallery" title="GALLERY">
+                  <Row>
+                    {eventData?.map((data) => (
+                      <Col md={4} className="mb-3">
+                        <Event event={data} />
+                      </Col>
+                    ))}
+                  </Row>
+                </Tab>
+                <Tab eventKey="feature" title="FEATURE VENUE">
+                  <Row>
+                    {eventData?.map((data) => (
+                      <Col md={4} className="mb-3">
+                        <Event event={data} />
+                      </Col>
+                    ))}
+                  </Row>
+                </Tab>
+                <Tab eventKey="design" title="DESIGN CONVETION">
+                  <Row>
+                    {eventData?.map((data) => (
+                      <Col md={4} className="mb-3">
+                        <Event event={data} />
+                      </Col>
+                    ))}
+                  </Row>
+                </Tab>
+                <Tab eventKey="individual" title="INDIVIDUAL">
+                  <Row>
+                    {eventData?.map((data) => (
+                      <Col md={4} className="mb-3">
+                        <Event event={data} />
+                      </Col>
+                    ))}
+                  </Row>
+                </Tab>
+              </Tabs>
+            </Col>
           </Row>
         </Container>
       </section>
