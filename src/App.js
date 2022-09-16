13f20a4a2efefb0b7c6e155545c9d3528b56d211
himanshu kumar <himanshu064@gmail.com>
2022-09-16 09:56:16 +0530

    Added measurement id in env also

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { Toaster } from "react-hot-toast";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import NavigationRoutes from "./routes";
 import ReactGA from "react-ga4";
+
 const queryClient = new QueryClient();
-const MEASUREMENTID = "G-M363XFWY9C";
+
+const MEASUREMENTID = process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "G-M363XFWY9C";
+
 ReactGA.initialize(MEASUREMENTID);
+
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
